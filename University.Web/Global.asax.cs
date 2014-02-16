782b9d0faa5f58bacc5f22239c2451e1c3d730db
NoFlaw <brentonbates@gmail.com>
2014-02-16 02:35:53 -0500

    Working o new DataLayer

--- University.Web/Global.asax.cs
+++ University.Web/Global.asax.cs
@@ -1,7 +1,11 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StructureMap;
+using University.Data.Initialization;
+using ResourceManager = University.Data.Initialization.ResourceManager;
 
 namespace University.Web
 {
@@ -9,11 +13,29 @@ namespace University.Web
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            //ObjectFactory Init
+            ResourceManager.Initialize();
+            ControllerBuilder.Current.SetControllerFactory(new StructureMapControllerFactory(new Container()));
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_End()
+        {
+            ResourceManager.Dispose();
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            ResourceManager.EndRequest(sender, e);
+        }
+
     }
 }
